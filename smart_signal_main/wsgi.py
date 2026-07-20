850d5a72d18dceae115c3df7126b60a0db51c35a
Ritu Chandrasekar <rituprithika.c18@gmail.com>
2026-07-21 01:57:56 +0530

    Fix vercel.json syntax and wsgi entry point

--- smart_signal_main/wsgi.py
+++ smart_signal_main/wsgi.py
@@ -3,3 +3,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smart_signal_main.settings')
 application = get_wsgi_application()
+app = application
