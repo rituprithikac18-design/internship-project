314fe96e5002104452815a7747df113d0938958a
Ritu Chandrasekar <rituprithika.c18@gmail.com>
2026-07-21 01:26:02 +0530

    updated

--- smart_signal_main/settings.py
+++ smart_signal_main/settings.py
@@ -92,7 +92,6 @@ WSGI_APPLICATION = "smart_signal_main.wsgi.application"
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
         conn_max_age=0,
         conn_health_checks=True,
     )
